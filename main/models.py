86983582c892361bcd0815c82468574fcbb3bead
Sherdos <110164213+Sherdos@users.noreply.github.com>
2025-09-14 19:57:12 +0600

    add templates

--- main/models.py
+++ main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Project(models.Model):
+
+    title = models.CharField(max_length=255)
+    description = models.TextField(max_length=255)
+    icon = models.ImageField(upload_to="icons/")
+
+    class Meta:
+        verbose_name = "Проект"
+        verbose_name_plural = "Проекты"
+
+    def __str__(self) -> str:
+        return f"{self.title}"
